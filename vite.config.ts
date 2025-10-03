d1abf1497900d0c4775914ef867cfe3c3ef4f0d5
danuniversalautomotive-maker <danrley.rodrigues@universalautomotive.com.br>
2025-10-03 16:10:32 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/site-forin/", // 👈 coloque o nome do repositório aqui
+  base: "/site-forin/",
   server: {
     hmr: true,
     watch: {
